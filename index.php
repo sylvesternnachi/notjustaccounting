1006cd9e3f39f8f35578ff4be16d2daebdb71321
sylvelity009 <sylvelity00@yahoo.com>
2018-03-23 19:00:55 +0100

    Just Accounting Static Page before Wordpress

---
+++ index.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<title> Home | NotJustAccounting </title>
+    <link href="https://fonts.googleapis.com/css?family=Amiko|Cabin|Lato|PT+Sans|Roboto" rel="stylesheet">
+    
+
+</head>
+<body>
+
+<header>
+    <div class="container">
+        
+        <div class="col-lg-3 col-md-3 hidden-sm hidden-xs">
+            <p style="magin:0pxx; padding:0px"><Span class="glyphicon glyphicon-home" style="color:#007cdf"></Span>   
+                 Prospect House, 2 Athanaeum Road, London </p>
+        </div>
+         <div class="col-lg-3 col-md-3 hidden-sm hidden-xs">
+             <p style="magin:0pxx; padding:0px"><Span class="glyphicon glyphicon-earphone
+" style="color:#007cdf"></Span>   
+                 020 8369 5111 
+             </p>
+        </div>
+         <div class="col-lg-3 col-md-3 hidden-sm hidden-xs">
+            <p style="magin:0pxx; padding:0px"><Span class="glyphicon glyphicon-envelope
+
+" style="color:#007cdf"></Span>   
+                 info@notjustaccounting.co.uk
+             </p>
+        </div>
+         <div class="col-lg-3 col-md-3 hidden-sm hidden-xs">
+            
+        </div>
+        
+    </div>
+	
+</header>
+    
+<div id="header">
+    <div class="container">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            <img src="img/notjustaccounting-logo.png" width="60%">
+        </div>
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+           
+            <div id="nav">
+                 <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+        </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+       <li class='<?php if($link == '/olivefoundation/index.php'){ echo 'active'; } ?> '><a href="index.php">HOME <span class="sr-only">(current)</span></a></li>
+        <li class="<?php if($link == '/olivefoundation/about-us.php'){ echo 'active'; } ?>"><a href="about-us.php">ABOUT US</a></li>
+       
+        <li class="<?php if($link == '/olivefoundation/foundation.php'){ echo 'active'; } ?>"><a href="foundation.php"> SERVICE</a></li>
+  
+        <li class="<?php if($link == '/olivefoundation/gallery.php'){ echo 'active'; } ?>"><a href="gallery.php">SOLUTION</a></li>
+      
+
+        <li class="<?php if($link == '/olivefoundation/contact.php'){ echo 'active'; } ?>"><a href="contact.php">CONTACT</a></li>
+		
+            <li>
+            <button class="btn btn-larg btn-warning" style="margin-top:5%">
+                <span style="font-weight:bold; font-style:italic; color:#243f5b">epos sofware</span>
+             </button></li>
+
+      </ul>
+    
+    
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+   
+            
+            </div>
+        
+        </div>
+    </div>
+</div>
+    
+<div
+
+<div id="banner">
+        <img src="img/banner1.jpg" width="100%" />
+</div>
+
+
+
+<div id="base-page">
+    <div class="container">
+                <p align="center">
+             Not Just Accounting - providing you with the service you need to solidify your success!
+            <button class="btn btn-large btn-default">learn more</button>
+        </p>
+    </div>
+   
+</div>
+    
+<div id="base-page2">
+
+    <div class="container">
+    
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                
+                <div class="b-box">
+                    <img src="img/accounting.jpg" width="100%" />
+                </div>
+        
+            </div>
+        
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="b-box">
+                    <img src="img/finance.jpg" width="100%" />
+                </div>
+            </div>
+        
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="b-box">
+                <img src="img/controller.jpg" width="100%" />
+                </div>
+            </div>
+        
+    </div>
+
+</div>
+
+<div id="content-container">
+
+    <div class="container">
+        <h2 align="center">Why Choose Us<br/><span class="cc-line"></span></h2>
+        
+        <p align="center">For a business to flourish what are the most important things to consider. A practical and revenue generating business plan, money, people, research and last but not the least account management. Only when all this is executed and applied correctly can you be sure about getting ahead with your business endeavour with flying colours.  <br /><br />
+
+This is exactly why people go for experts like Not Just Accounting. They are affordable and the best kind of accounting firms in North London UK. They will assist you and ascertain growth of your business with excellent account handling services.   <br /> Their proficient assistance becomes even more valuable because of the flexibility of services offered. You can hire them part time or outsource accounting projects and expect a detailed, well-managed accounts and bookkeeping services. <br />
+
+For the ultimate solution to all your accounts and finances that leads to progressive business, approach only experts like Not Just Accounting. They are reliable and most updated London account service providers. 
+You may not find a better accounting firm in North London UK</p>
+        
+      <p align="center"><img src="img/icon-1.png" width="30%" /></p> 
+    </div>
+    
+</div>
+
+<div id="service-base">
+    <div class="container">
+        <h2>Our Services<br/><span class="cc-line"></span></h2>
+    </div>
+</div>
+
+<div id="icon-base">
+
+    <div class="container" style="margin-top:3%">
+        
+    <div class="row">
+    
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            <div class="icon-div">
+                <p align="center">
+                <img src="img/payroll.jpg">
+                Payroll</p>
+            </div>
+        </div>
+        
+         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+             
+             <div class="icon-div">
+                <p align="center">
+                <img src="img/booking-keeping.jpg">
+                Book keeping</p>
+            </div>
+        
+        </div>
+        
+         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+             
+            <div class="icon-div">
+                <p align="center">
+                <img src="img/vat.jpg">
+                VAT</p>
+            </div>
+        
+        </div>
+        
+   
+        
+        
+        
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                
+            <div class="icon-div">
+                <p align="center">
+                <img src="img/management.jpg">
+                Business Accounting
+                </p>
+            </div>
+                
+            </div>
+            
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                
+                <div class="icon-div">
+                <p align="center">
+                <img src="img/business-management.jpg">
+                Business Plans
+                </p>
+                </div>
+                
+            </div>
+            
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                
+                <div class="icon-div">
+                <p align="center">
+                <img src="img/financial-control.jpg">
+                Financial Control
+                </p>
+                </div>
+                    
+            </div>
+            
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                
+                <div class="icon-div">
+                <p align="center">
+                <img src="img/training.jpg">
+                Training
+                </p>
+                </div>
+                    
+            </div>
+        
+        </div>
+        
+    
+    </div>
+</div>
+
+<div id="footer">
+    
+    <div class="container">
+        
+        <div class="row">
+
+        <div class="col-lg-4 col-md-4 col-sm-12 col-md-12">
+            
+            <div class="footer-box">
+                <h2>About Us</h2>
+                <p>Established in 2005 Not Just Accounting (NJA) has worked with small business clients across the UK – both sole traders and limited companies. </p>
+            </div>
+        
+        </div>
+        
+         
+        <div class="col-lg-4 col-md-4 col-sm-12 col-md-12">
+            
+            <div class="footer-box">
+                
+                <h2>Schedule a Consultation</h2>
+                
+            
+            </div>
+        
+        </div>
+        
+         
+        <div class="col-lg-4 col-md-4 col-sm-12 col-md-12">
+            <div class="footer-box">
+                
+                 <h2>Our Focus Areas</h2>
+                <ul>
+                    <li>  <a href="#">Concierge Financial Care</a> </li>
+                    <li><a href="#"> Entrepreneurs</a></li>
+                    <li> <a href="#">    Outsourced Controller</a></li>
+                </ul>
+                
+                 <h2>Solutions</h2>
+                
+                <ul>
+                    <li>  <a href="#">Industries</a> </li>
+                    <li><a href="#"> Small to Mid-Sized Businesses</a></li>
+                    <li> <a href="#">    Start Ups</a></li>
+                </ul>
+                 <h2>&nbsp;</h2>
+                <ul>
+                    <li>  <a href="#">Bookkeeping for Small Business</a> </li>
+                    <li><a href="#"> Financial Advisor London</a></li>
+                 
+                </ul>
+            </div>
+            
+            </div>
+        
+        
+        
+        
+       
+            
+        </div>
+        
+         <div class="row" style="border-top:1px solid #339cd7; padding-top:2%">
+            
+            <div class="col-lg-9 col-md-9 col-sm-6 col-xs-12">
+            <div class="down-footer">
+             <p>Not Just Accounting Ltd, Prospect House, 2 Athanaeum Road, London, Phone: 020 8369 5111 <br /> Not Just Accounting &copy; <?php echo date("Y"); ?>. All Rights Reserved.
+</p>
+            </div>
+            </div>
+             <div class="col-lg-3 col-md-3 col-sm-6 hidden-xs">
+                <div class="down-footer">
+                        <img src="img/fb.png">
+                        <p> Design by vPlusMEDIA</p>
+                 </div>
+             </div>
+            
+        </div>
+        
+    
+    </div>
+    
+</div>
+</body>
+</html>
